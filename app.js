5d3064f1e9adddb31258b4c767c1ca832a7d82af
RichardAbsin98 <rabsin@uci.edu>
2022-02-27 17:02:05 -0800

    Checkpoint for challenge 5 & 6

--- app.js
+++ app.js
@@ -21,16 +21,17 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
 
-
-
-
-
-
-
-
-
-
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    contactContent: contactContent
+  })
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
